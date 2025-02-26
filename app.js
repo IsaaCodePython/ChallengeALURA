2566f61fce237bc066947c64cc7ab848b9a90e52
IsaaCodePython <isaactrenard@gmail.com>
2025-02-26 14:57:39 -0300

    chamado ao html linha 27 para mostrar listadeamigos

--- app.js
+++ app.js
@@ -4,11 +4,11 @@ titulo.innerHTML = 'Jogo do amigo secreto';
 let subtitulo = document.querySelector ('h2');
 subtitulo.innerHTML = 'digite o nome do seus amigos';
 // armazenagem da ID Lista de amigos nomes
-let listadeAmigos []
+let listadeAmigos = [];
 // funções 
 function adicionarPana() {
     console.log ('o botao foi clicado');
     let nomeInput = document.getElementById("amigo");
     let nome = nomeInput.value.trim();
-    let listaDePanas = [];
+    let lista = document.getElementById("listaAmigos");
 }
