c25b783b3c814705bd691112c435fe18ca28cf0d
IsaaCodePython <isaactrenard@gmail.com>
2025-02-26 14:41:34 -0300

    adicionar 3

--- app.js
+++ app.js
@@ -8,6 +8,6 @@ subtitulo.innerHTML = 'digite o nome do seus amigos';
 function adicionarPana() {
     console.log ('o botao foi clicado');
     let adicionarPana = document.getElementById("adicionar");
-    let adicionar = adicionar.value;
+    let adicionarPana = adicionar.value;
     let listaDePanas = [];
 }
