4bc3f73a502d66ef1176e2cb9e7a2b7dffae6f53
IsaaCodePython <isaactrenard@gmail.com>
2025-02-26 14:55:36 -0300

    variavel pra adicionar amigos

--- app.js
+++ app.js
@@ -3,7 +3,8 @@ let titulo = document.querySelector ('h1');
 titulo.innerHTML = 'Jogo do amigo secreto';
 let subtitulo = document.querySelector ('h2');
 subtitulo.innerHTML = 'digite o nome do seus amigos';
-
+// armazenagem da ID Lista de amigos nomes
+let listadeAmigos []
 // funções 
 function adicionarPana() {
     console.log ('o botao foi clicado');
