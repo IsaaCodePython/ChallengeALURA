0758797fbd572b776bdd58e540704b79a0117f84
IsaaCodePython <isaactrenard@gmail.com>
2025-02-26 12:29:28 -0300

    subtitulo

--- app.js
+++ app.js
@@ -1,11 +1,13 @@
+//titulos e subtitulos
 let titulo = document.querySelector ('h1');
 titulo.innerHTML = 'Jogo do amigo secreto';
 let subtitulo = document.querySelector (h2);
 subtitulo.innerHTML = 'digite o nome do seus amigos';
-let listaDePanas = [];
-let nomeInput = document.getElementById("nomeInput").value;
 
+// funções 
 function adicionarPana() {
     console.log ('o botao foi clicado');
     let nome = nomeInput.value;
+    let listaDePanas = [];
+let nomeInput = document.getElementById("nomeInput").value;
 }
