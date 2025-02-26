07418962fa084099569a0c1409dc9c077f14e69b
IsaaCodePython <isaactrenard@gmail.com>
2025-02-26 15:58:13 -0300

    lista na tela

--- app.js
+++ app.js
@@ -1,20 +1,31 @@
-//titulos e subtitulos
-let titulo = document.querySelector ('h1');
+// Títulos e subtítulos
+let titulo = document.querySelector('h1');
 titulo.innerHTML = 'Jogo do amigo secreto';
-let subtitulo = document.querySelector ('h2');
-subtitulo.innerHTML = 'digite o nome do seus amigos';
-// armazenagem da ID Lista de amigos nomes
+
+let subtitulo = document.querySelector('h2');
+subtitulo.innerHTML = 'Digite o nome dos seus amigos';
+
+// Armazenamento da lista de amigos
 let listadeAmigos = [];
-// funções 
+
 function adicionarAmigo() {
-    console.log ('o botao foi clicado');
+    console.log('O botão foi clicado');
+
     let nomeInput = document.getElementById("amigo");
     let nome = nomeInput.value.trim();
     let lista = document.getElementById("listaAmigos");
 
     if (!nome) {
-        alert("Por favor adicione um nome, para continuar com o sorteio");
+        alert("Por favor, adicione um nome para continuar com o sorteio.");
         return;
     }
+
+    // Adiciona o nome à lista de amigos
+    listadeAmigos.push(nome);
+
+    // Criar um novo elemento <li>
+    let item = document.createElement("li");
+    item.textContent = nome;
+
+  
 }
-listadeAmigos.push(nome);
