89244a330e45e4f552d9a547b9be3266ea2b332e
IsaaCodePython <isaactrenard@gmail.com>
2025-02-26 11:43:04 -0300

    correcao =

--- app.js
+++ app.js
@@ -1,7 +1,7 @@
 let titulo = document.querySelector ('h1');
-titulo.innerHTML : 'Jogo do amigo secreto';
+titulo.innerHTML = 'Jogo do amigo secreto';
 let subtitulo = document.querySelector (h2);
-subtitulo.innerHTML : 'digite o nome do seus amigos';
+subtitulo.innerHTML = 'digite o nome do seus amigos';
 let listaDePanas = [];
 let nomeInput = document.getElementById("nomeInput").value;
 
